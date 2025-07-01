dea6470ff49c0dec4f28b2c1e660eabea5a64782
Matt Adams <19655503+d3xbot@users.noreply.github.com>
2025-07-01 13:53:44 -0400

    Update dns-reset.sh

--- DNS-Reset/dns-reset.sh
+++ DNS-Reset/dns-reset.sh
@@ -3,18 +3,21 @@
 mvers=$(sw_vers -productVersion | cut -c 1-2)
 
 case mvers in
-	13 | 14 | 15)
+	14 | 15 | 26)
 		dscacheutil -flushcache
 		killall -HUP mDNSResponder
+  		echo "Flushed DNS Cache"
 	;;
-	11 | 12)
+	11 | 12 | 13)
 		dscacheutil -flushcache
 		killall -HUP mDNSResponder
 		echo "This version of macOS is not supported. Please upgrade your Mac."
+  		echo "Flushed DNS Cache"
 	;;
 	10)
 		killall -HUP mDNSResponder
 		echo "This version of Mac OS is not supported. Please upgrade your Mac."
+    		echo "Flushed DNS Cache"
 	;;
 esac
 exit 0
